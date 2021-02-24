4a001bc779ca9627e5011319aac25cea347b6c0b
anfornee <anthonyp@ichop.org>
2021-02-24 15:33:05 -0800

    commit

--- script.js
+++ script.js
@@ -1,86 +1,71 @@
-////////////* Contact Form *///////////////
+/// Contact Form ///
 $(document).ready(function () {
   $('.submit').click(function (event) {
-
-    let email = $('.email').val();
-    let message = $('.message').val();
-    var statusElm = $('.status');
-    statusElm.empty();
+    const email = $('.email').val()
+    const message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
 
     if (email.length > 5 && email.includes('@') && email.includes('.')) {
-      statusElm.remove;
-    } else {
-      event.preventDefault();
-      statusElm.append('*Invalid email </br>');
-    };
-
-    if (message) {
-      statusElm.remove;
+      statusElm.remove()
     } else {
       event.preventDefault()
-      statusElm.append('*Must include a message </br>');
-    };
+      statusElm.append('*Invalid email </br>')
+    }
 
+    if (message) {
+      statusElm.remove()
+    } else {
+      event.preventDefault()
+      statusElm.append('*Must include a message </br>')
+    }
   })
-});
+})
 
-////////////* Sticky Header - Scroll Button Hide/Reveal - menuIcon Invert *///////////////
-window.onscroll = function () { headerClass() };
+/// Mobile Ham Menu ///
+const hamMenu = document.getElementById('hamMenu')
+hamMenu.addEventListener('click', () => {
+  const mobileNav = document.getElementById('navMenu')
+  if (hamMenu.classList.contains('open')) {
+    hamMenu.classList.remove('open')
+    mobileNav.classList.remove('open')
+  } else {
+    hamMenu.classList.add('open')
+    mobileNav.classList.add('open')
+  }
+})
 
-var header = document.getElementById("header");
+/// Sticky Header - Scroll Button Hide/Reveal - menuIcon Invert ///
+window.onscroll = function () { headerClass() }
 
-var sticky = header.offsetTop;
+var header = document.getElementById('header')
 
-let scrollIcon = document.getElementById('moreStuff');
+var sticky = header.offsetTop
 
-let menuIcon = document.getElementById('menuIcon');
+const scrollIcon = document.getElementById('moreStuff')
 
-function headerClass() {
+function headerClass () {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-    scrollIcon.classList.add("scrollButtonHide");
-    menuIcon.classList.add("menuIconInvert");
+    header.classList.add('sticky')
+    scrollIcon.classList.add('scrollButtonHide')
+    hamMenu.classList.add('black')
   } else {
-    header.classList.remove("sticky");
-    scrollIcon.classList.remove("scrollButtonHide");
-    menuIcon.classList.remove("menuIconInvert");
+    header.classList.remove('sticky')
+    scrollIcon.classList.remove('scrollButtonHide')
+    hamMenu.classList.remove('black')
   }
-};
+}
 
-////////////* Hover Over Scroll Icon *///////////////
-function isHover(e) {
-  return (e.parentElement.querySelector(':hover') === e);
-};
+/// Hover Over Scroll Icon ///
+function isHover (e) {
+  return (e.parentElement.querySelector(':hover') === e)
+}
 
-document.addEventListener('mousemove', function checkHover() {
-  var hovered = isHover(scrollIcon);
+document.addEventListener('mousemove', function checkHover () {
+  var hovered = isHover(scrollIcon)
   if (hovered) {
-    scrollIcon.classList.add("pulse");
+    scrollIcon.classList.add('pulse')
   } else {
-    scrollIcon.classList.remove("pulse");
+    scrollIcon.classList.remove('pulse')
   }
-});
-
-////////////* Screen Size Nav Response *///////////////
-menuIcon.addEventListener('click', function rotateMenu(e) {
-  if (menuIcon.className == "clicked") {
-    menuIcon.classList.toggle("clicked");
-  } else {
-    menuIcon.classList.toggle("clicked");
-  }
-  e.preventDefault();
-});
-
-var nav = document.querySelector("nav"),
-  navToggle = document.querySelector(".skip");
-if (navToggle) {
-  navToggle.addEventListener("click",
-    function (e) {
-      if (nav.className == "open") {
-        nav.className = "";
-      } else {
-        nav.className = "open";
-      }
-      e.preventDefault();
-    }, false);
-};
+})
